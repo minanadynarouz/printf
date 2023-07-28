ea6308cf04d08456959ddfaf8365a68306e9f36f
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-28 15:38:58 -0700

    amend

---
+++ main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int main(void)
+{
+
+    _printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+
+    return (0);
+}
