063c0b323489442391374ebfdcb29f46409367a7
minanadynarouz <mina.n.narouz@gmail.com>
2023-07-28 19:31:53 -0700

    submit

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-int main(void)
-{
-
-    _printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
-
-    return (0);
-}
